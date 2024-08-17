91d2a9f0fda9f64fc78f814e73932a84e020c863
ahmednule <nuleahmed6@gmail.com>
2024-08-17 10:05:48 +0300

    chnged, the crsf blocks'

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = '#*k)9t8vzu)4j2hr=l@)emxs^ev%6z5)ri$q4g4%nb^n+pr37y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://online-bookstore-e7km.onrender.com', 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://online-bookstore-e7km.onrender.com']
 
 
 # Application definition

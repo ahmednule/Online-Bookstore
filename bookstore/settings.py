2c9fe612baf8e8f91b9b6a70d7ac4bd03be32e85
ahmednule <nuleahmed6@gmail.com>
2024-08-17 11:19:30 +0300

    was making the file to be responsive

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#*k)9t8vzu)4j2hr=l@)emxs^ev%6z5)ri$q4g4%nb^n+pr37y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
